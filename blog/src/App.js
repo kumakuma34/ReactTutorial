613cbb6c14a10c2d4c521a11fd95743939d1337e
kumakuma34 <qgqg264@gmail.com>
2022-01-24 22:27:04 +0900

    feat: add event handler on quiz
    Signed-off-by: kumakuma34 <qgqg264@gmail.com>

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,6 +2,15 @@ import React from "react";
 import "./App.css";
 
 function App() {
+    const handleClick = (e) =>{
+        const answer = e.target.value;
+        if(answer === "스페이스 엑스"){
+            alert("정담");
+        }
+        else{
+            alert("오답");
+        }
+    }
     return (
         <div className = "container">
             <div className = "app">
@@ -10,10 +19,10 @@ function App() {
                     <div className = "question-text">일론 머스크의 우주 탐사 기업 이름은?</div>
                 </div>
                 <div className = "answer-section">
-                    <button>스페이스 엑스</button>
-                    <button>테슬라</button>
-                    <button>보링 컴퍼니</button>
-                    <button>솔라시티</button>
+                    <button onClick = {handleClick} value = "스페이스 엑스">스페이스 엑스</button>
+                    <button onClick = {handleClick} value = "테슬라">테슬라</button>
+                    <button onClick = {handleClick} value = "보링 컴퍼니">보링 컴퍼니</button>
+                    <button onClick = {handleClick} value = "솔라시티">솔라시티</button>
                 </div>
             </div>
         </div>
